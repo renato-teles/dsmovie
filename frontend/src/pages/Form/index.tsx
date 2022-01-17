6c04fe8001ecd16fb0bb4dee90ec5667ca832068
Renato Teles <rtelesp@gmail.com>
2022-01-16 21:40:13 -0300

    Show score

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,5 @@
 import FormCard from 'components/FormCard';
-import { Link, useParams } from 'react-router-dom';
-import { Movie } from 'types/movies';
+import { useParams } from 'react-router-dom';
 import './styles.css'
 
 function Form(){
